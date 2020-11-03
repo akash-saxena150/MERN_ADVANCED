be575cefa519a31224fecd91c7d307936c24fe63
aks-gsg <akash@getsetgo.fitness>
2020-11-03 13:24:16 +0530

    Adding the FE files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./theme";
 
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./Component/Routes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth='xl' style={{ height: "100vh" }}>
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </Router>
   );
 }
 
